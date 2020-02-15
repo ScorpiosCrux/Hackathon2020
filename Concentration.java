d3b39ebaa17bd5d041ff11e1d6ca820caa8f2d77
ScorpiosCrux <chentyler52@gmail.com>
2020-02-15 15:07:22 -0700

    Add files via upload

---
+++ Concentration.java
@@ -0,0 +1,18 @@
+public class Concentration {
+
+	
+	String[][] infosec = new String[][]{
+		  {"1", "CPSC 329"},
+		  {"1", "CPSC 441"},
+		  {"1", "CPSC 525"},
+		  {"1", "CPSC 526"},
+		  {"1", "CPSC 418", "PMATH 418", "MATH 318"},
+		  {"1", "CPSC 502.04", "CPSC 503.04", "CPSC 527", "CPSC 528", "CPSC 530", "CPSC 577", "SENG 521"},
+	};
+	
+		
+
+	public String[][] getConcentration(double concentration) {
+		return infosec;
+	}
+}
