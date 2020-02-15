0442e080fcd286e7dfd359ff78903464a8b68a9b
volknerps <32214781+volknerps@users.noreply.github.com>
2020-02-15 16:24:50 -0700

    requisites update
    add stats and seng

--- Requisites.java
+++ Requisites.java
@@ -18,9 +18,9 @@ public class Requisites {
 	    preRequisites.put("CPSC359", "CPSC355 | CPSC279");
 	    preRequisites.put("CPSC449", "CPSC331 | PHIL279");    
 	    preRequisites.put("CPSC457", "CPSC331 | CPSC359");
-	    preRequisites.put("MATH211", "");
 	    preRequisites.put("MATH265", "");
 	    preRequisites.put("STAT213", "");
+	    preRequisites.put("SENG300", "CPSC331");
 	    
 	    antiRequisites.put("CPSC231","CPSC215 | CPSC217 | CPSC235 | DATA211 | ENGG233 | ENCM339");
 	    antiRequisites.put("CPSC233","CPSC219 | CPSC235 | ENEL497 | ENCM493");
@@ -33,7 +33,9 @@ public class Requisites {
 	    antiRequisites.put("CPSC413","");
 	    antiRequisites.put("CPSC449","");
 	    antiRequisites.put("CPSC457","");
-	    antiRequisites.put("MATH265","MATH249 | MATH275");
+	    antiRequisites.put("MATH265", "MATH249 | MATH275");
+	    antiRequisites.put("STAT213", "STAT205 | STAT327 | POLI399 | PSYC300 | PSYC301 | PSYC312 | SOCI311");
+	    antiRequisites.put("SENG300", "SENG301 | SENG311 | SENG403 | SENG411 | CPSC301 | CPSC333 | CPSC451 | ENSF480")
 	}
 	
 	
