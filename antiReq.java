c3bec89c24fc1c726ac91dce89c1e016adac74d0
SunnyM17 <47331378+SunnyM17@users.noreply.github.com>
2020-02-15 15:48:03 -0700

    Update and rename Courses.java to antiReq.java

---
+++ antiReq.java
@@ -0,0 +1,18 @@
+public class antiReq {
+	public static void main(String[]] args){
+		HashMap<String,String> antiReq = new HashMap<String,String>();
+		antiReq.put("CPSC231","CPSC215 | CPSC217 | CPSC235 | DATA211 | ENGG233 | ENCM339");
+		antiReq.put("CPSC233","CPSC219 | CPSC235 | ENEL497 | ENCM493");
+		antiReq.put("MATH211","MATH213");
+		antiReq.put("MATH271","");
+		antiReq.put("CPSC331","CPSC319");
+		antiReq.put("CPSC313","");
+		antiReq.put("CPSC355","CPSC265 | ENCM369");
+		antiReq.put("CPSC359","CPSC325 | CPSC455 | ENCM415");
+		antiReq.put("CPSC413","");
+		antiReq.put("CPSC449","");
+		antiReq.put("CPSC457","");
+		antiReq.put("MATH265","MATH249 | MATH275");				
+		
+	}
+}
