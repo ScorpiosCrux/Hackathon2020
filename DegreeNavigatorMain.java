f64613d2ca08a23005987a0a464e2aa16236ccf2
ScorpiosCrux <chentyler52@gmail.com>
2020-02-15 19:13:04 -0700

    Add files via upload

---
+++ DegreeNavigatorMain.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DegreeNavigatorMain {
+
+	private Majors major;
+	private Concentration concentration;
+	private Requisites requisites;
+	
+	public DegreeNavigatorMain(){
+		this.major = new Majors("computer science", "information security");
+		this.concentration = new Concentration();
+		this.requisites = new Requisites(); 
+		System.out.println(this.requisites.getPreRequisites().keySet());
+		analyze(requisites.getPreRequisites());
+		
+	}
+	
+	
+	public void analyze(HashMap<String, String> requisites) {
+		ArrayList<String> allPrerequisites = new ArrayList<String>();
+		for (String i : requisites.values()) {
+			i = i.replace(" | ", " ");
+			String[] array = i.split(" ");
+			for (int j = 0; j < array.length; j++) {
+				if (array[j].equals(""))
+					break;
+				allPrerequisites.add(array[j]);
+			}
+		}
+		for (String i : requisites.keySet()) {
+			int occurrences = Collections.frequency(allPrerequisites, i);
+			System.out.println(i + ": " + occurrences);
+		}
+		
+	}
+
+}
